4da0a19506ee7b303892c59d8e8aaf66206639de
Nigel Dcruz <nigeldcruz7@gmail.com>
2020-03-29 20:46:41 +0530

    SCSS and file loaders added to the webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,44 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: ['@babel/polyfill', './public/router/Router.js'],
-    mode: "development",
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'public'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ["@babel/preset-env"]
-                }
-            }
+  entry: ["@babel/polyfill", "./index.js"],
+  mode: "development",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "public")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        query: {
+          presets: ["@babel/preset-env"]
+        }
+      }
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader"
         ]
-    },
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader"
+      }
+    ]
+  },
 };
