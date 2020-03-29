6abb3be5dc68c48f376faa0e0b6dcc362c8f5ac6
Peter <lunfan@gmx.net>
2020-03-29 16:09:46 +0200

    added mode (development)

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
     entry: ['@babel/polyfill', './public/router/Router.js'],
+    mode: "development",
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'public'),
